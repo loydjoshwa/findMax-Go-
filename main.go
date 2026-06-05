350e3765e7a0b2fdbfa4679ff9695904124fa226
loy <loydjoshwad@gmail.com>
2026-06-05 10:50:40 +0530

    update

--- main.go
+++ main.go
@@ -7,15 +7,16 @@ func findmax(arr []int) int {
 
 	for i := 1; i < len(arr); i++ {
 		if max<arr[i]{
+			
 			max=arr[i]
+
 		}
-		
 	}
 	return max
 }
 func main() {
 	fmt.Println("find maximum in an array in a")
 	array:=[]int{20,30,30,40,900,40,1}
-	a:=findmax(array)
-	fmt.Println("max:",a)   
+	a:=findmax(array)    
+	fmt.Println("max:",a)     
 }
