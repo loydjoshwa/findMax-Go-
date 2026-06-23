c09a57c40a576d242110c1c664201c41e3e291e0
loy <loydjoshwad@gmail.com>
2026-06-23 10:32:42 +0530

    practice graph

--- main.go
+++ main.go
@@ -2,21 +2,22 @@ package main
 
 import "fmt"
 
-func findmax(arr []int) int {
-	max:=arr[0]
+func hasCycle( graph map[string]string)
 
-	for i := 1; i < len(arr); i++ {
-		if max<arr[i]{
-			 
-			max=arr[i]    
+func main() {
+	fmt.Println("adjacency matrix")
 
-		}
-	}  
-	return max
-}     
-func main() {                
-	fmt.Println("find maximum in an array")
-	array:=[]int{20,30,30,40,900,40,1} 
-	a:=findmax(array)   
-	fmt.Println("max:",a)            
+	
+	graph:=map[string][]string{
+		"A":{"B","C"},
+		"B":{"A","C"},
+		"C":{"A","D"},
+		"D":{"B"},
+	}
+
+	for c, v := range graph {
+		fmt.Println(c,"->",v)
+	}
+
+	fmt.Println("A is conected to",graph["A"])
 }
