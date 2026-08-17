b9220e568c3a3c041f83ca030ae261b40b98e928
loy <loydjoshwad@gmail.com>
2026-08-17 09:20:41 +0530

    updated

--- main.go
+++ main.go
@@ -13,10 +13,10 @@ func main() {
 		"C":{"A","D"},   
 		"D":{"B"},     
 	}     
-	for c, v := range graph {
+	for c, v := range graph { 
 		fmt.Println(c,"->",v)        
 	}          
    
 	fmt.Println("A is conected to",graph["A"])  
-}        
+}         
  
